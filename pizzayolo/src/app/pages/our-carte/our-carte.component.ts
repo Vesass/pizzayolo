440c2a864b8d01ca774ae29c241adba8c2063d44
Maxime GodartHupet <godarthupetmaxime@outlook.com>
2018-10-25 10:06:42 +0200

    pizza mise en suspend

--- pizzayolo/src/app/pages/our-carte/our-carte.component.ts
+++ pizzayolo/src/app/pages/our-carte/our-carte.component.ts
@@ -12,14 +12,68 @@ import { Router } from '@angular/router';
 export class OurCarteComponent implements OnInit {
 
   pizzas: any;
+  ingredients: any;
 
   constructor(private ingredientService: IngredientService, private router: Router) { }
 
   ngOnInit() {
 
+    // let dragonne;
+    // this.ingredientService.getIngredient().subscribe((reponse) => {
+    //   console.log(reponse);
+    //   this.ingredients = reponse;
+    //   this.ingredients.forEach(element => {
+
+    //     dragonne = element.id_ing;
+
+    //   });
+    // },
+    //   error => console.log(error)
+    // );
+
+
+
     this.ingredientService.getPizza().subscribe((reponse) => {
-      //  console.log(reponse);
+      console.log(reponse);
       this.pizzas = reponse;
+      let dragon = {};
+
+
+      this.pizzas.forEach(element => {
+        console.log("pizza")
+
+
+        // element.ingredients.forEach(idIngCollPizza => {
+
+        //   /* 
+        //             for (let i = 0; i < this.ingredients.length; i++) {
+        //               this.ingredients */
+
+        //   this.ingredients.forEach(item => {
+        //     if (idIngCollPizza == item.id_ing) {
+
+        //       console.log(item.name)
+
+        //       idIngCollPizza = item.name
+        //       return idIngCollPizza
+
+        //     }
+
+
+        //   });
+
+
+
+
+
+        //   //   console.log(test.name)
+        //   // }
+
+
+        // });
+      });
+
+
     },
       error => console.log(error)
     );
